5cbf0d07d27868b230441e25b4219842efdcb142
Igor <129342980+Igorio29@users.noreply.github.com>
2024-09-26 11:38:06 +0000

    banco de dados sincronizado

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,22 @@
 import "dotenv/config";
 import express from "express";
+import mongoose from "mongoose";
+
+async function connectToDatabase() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to the database");
+  } catch (error) {
+    console.error("Error connecting to the databse: ", error);
+    process.exit(1);
+  }
+}
 
 const app = express();
 
 app.listen(3000, () => {
-  console.log("Server is runnig on port 3000");
+  console.log("Server is running on port 3000");
+  connectToDatabase();
 });
+
+//senha: Z8iJOz5EWohnayzj
