6678ee86b23daac5cd6361dc876bea6204880641
Igor <129342980+Igorio29@users.noreply.github.com>
2024-10-06 17:42:24 +0000

    CompleteCRUD

--- backend/server.js
+++ backend/server.js
@@ -53,7 +53,61 @@ app.get("/api/v1/books/:id", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
+app.put("/api/v1/books/:id", async (req, res) => {
+  const { id } = req.params;
+  const { title, subtitle, author, genre, cover } = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ sucess: false, error: "Invalid ID" });
+  }
+
+  try {
+    const book = await Book.findById(id);
+
+    if (!book) {
+      return res.status(404).json({ success: false, error: "Book not found" });
+    }
+
+    const updatedBook = await Book.findByIdAndUpdate(
+      id,
+      { title, subtitle, author, genre, cover },
+      { new: true }
+    );
+
+    res.status(200).json({
+      success: true,
+
+      data: updatedBook,
+    });
+  } catch (error) {
+    console.error("Error updating book: ", error);
+    res.status(500).json({ success: false, error: "Error updating book" });
+  }
+});
+
+app.delete("/api/v1/books/:id", async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ sucess: false, error: "Invalid ID" });
+  }
+
+  try {
+    const book = await Book.findById(id);
+
+    if (!book) {
+      return res.status(404).json({ success: false, error: "Book not found" });
+    }
+
+    await Book.findByIdAndDelete(id);
+
+    res.status(200).json({ success: true, data: {} });
+  } catch (error) {
+    console.error("Error deleting book: ", error);
+    res.status(500).json({ success: false, error: "Error deleting book" });
+  }
+});
+app.listen(3500, () => {
   console.log("Server is running on port 3000");
   connectToDatabase();
 });
